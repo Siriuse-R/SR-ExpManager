dbedd173fd499f40ee75d65c36244ceea741bfe5
Siriuse-R <admin@dejiplus.net>
2013-07-16 01:38:13 +0900

    かまど、ブロック破壊時の経験値取得に対応

--- src/main/java/net/siriuser/SRExpManager/ExpListener.java
+++ src/main/java/net/siriuser/SRExpManager/ExpListener.java
@@ -16,9 +16,11 @@ import org.bukkit.entity.Wolf;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.inventory.FurnaceExtractEvent;
 
 public class ExpListener implements Listener {
 
@@ -32,45 +34,67 @@ public class ExpListener implements Listener {
     }
 
     @EventHandler(priority = EventPriority.HIGH)
-    public void OriginalGiveExp (final EntityDeathEvent event) {
+    public void EntityGiveExp (final EntityDeathEvent event) {
         final LivingEntity ent = event.getEntity();
         final int dropExp = event.getDroppedExp();
 
-        if (ent.getLastDamageCause().equals(null)) return;
+        if (ent.getLastDamageCause() == (null)) return;
 
         final EntityDamageEvent cause = ent.getLastDamageCause();
-        if (cause instanceof EntityDamageByEntityEvent) return;
+        if (cause instanceof EntityDamageByEntityEvent) {
+            Entity killer = ((EntityDamageByEntityEvent) cause).getDamager();
 
-        Entity killer = ((EntityDamageByEntityEvent) cause).getDamager();
+            if (ent.getType().equals(EntityType.ENDER_DRAGON)) {
+                return;
 
-        if (ent.getType().equals(EntityType.ENDER_DRAGON)) {
-            return;
+            } else {
 
-        } else {
+                if (killer instanceof Player) {
+                    Player killerP = (Player)killer;
 
-            if (killer instanceof Player) {
-                Player killerP = (Player)killer;
+                    killerP.giveExp(dropExp);
+                    killerP.sendMessage(Util.coloring("&a[Exp] &2" + ent.getType().getName() + "&eを倒し" + dropExp + "経験値を獲得。"));
+                    event.setDroppedExp(0);
 
-                killerP.giveExp(dropExp);
-                killerP.sendMessage(Util.coloring("&a[Exp] &2" + ent.getType().getName() + "&eを倒し" + dropExp + "経験値を獲得。"));
-                event.setDroppedExp(0);
+                } else if (killer instanceof Wolf && ((Wolf) killer).isTamed()) {
+                    Wolf killerW = (Wolf)killer;
+                    Player player = (Player) killerW.getOwner();
 
-            } else if (killer instanceof Wolf && ((Wolf) killer).isTamed()) {
-                Wolf killerW = (Wolf)killer;
-                Player player = (Player) killerW.getOwner();
+                    player.giveExp(dropExp + 5);
+                    player.sendMessage(Util.coloring("&a[Exp] &2" + ent.getType().getName() + "&eを狼が倒し" + dropExp + "経験値を獲得。&b (狼ボーナス: +5)"));
+                    event.setDroppedExp(0);
 
-                player.giveExp(dropExp + 5);
-                player.sendMessage(Util.coloring("&a[Exp] &2" + ent.getType().getName() + "&eを狼が倒し" + dropExp + "経験値を獲得。&b (狼ボーナス: +5)"));
-                event.setDroppedExp(0);
+                } else if (killer instanceof Arrow) {
+                    Arrow arrow = (Arrow)killer;
+                    Player killerP = (Player)arrow.getShooter();
 
-            } else if (killer instanceof Arrow) {
-                Arrow arrow = (Arrow)killer;
-                Player killerP = (Player)arrow.getShooter();
-
-                killerP.giveExp(dropExp);
-                killerP.sendMessage(Util.coloring("&a[Exp] &2" + ent.getType().getName() + "&eを倒し" + dropExp + "経験値を獲得。"));
-                event.setDroppedExp(0);
+                    killerP.giveExp(dropExp);
+                    killerP.sendMessage(Util.coloring("&a[Exp] &2" + ent.getType().getName() + "&eを倒し" + dropExp + "経験値を獲得。"));
+                    event.setDroppedExp(0);
+                }
             }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void BlockBreakGiveExp (final BlockBreakEvent event) {
+        final Player player = event.getPlayer();
+        final int dropExp = event.getExpToDrop();
+
+        if (dropExp != 0) {
+            player.giveExp(dropExp);
+            player.sendMessage(Util.coloring("&a[Exp] &2" + event.getBlock().getType().toString() + "&eを破壊し" + dropExp + "経験値を獲得。"));
+        }
+    }
+
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onFurnaceExtract(final FurnaceExtractEvent event) {
+        final Player player = event.getPlayer();
+        final int dropExp = event.getExpToDrop();
+
+        if (dropExp != 0) {
+            player.giveExp(dropExp);
+            player.sendMessage(Util.coloring("&a[Exp] &2" + "&eかまどから" + dropExp + "経験値を獲得。"));
         }
     }
 }
